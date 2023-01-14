81fcc2b9fbeb572c6280175cf8641c69814425a7
Kenneth Tomas <kennethtomas.it@gmail.com>
2023-01-14 12:56:54 -0600

    inicializacion de proyecto

---
+++ src/app/pages/nopagesfound/nopagesfound.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagesfound',
+  templateUrl: './nopagesfound.component.html',
+  styleUrls: ['./nopagesfound.component.css']
+})
+export class NopagesfoundComponent  {
+
+  anio =  new Date().getFullYear();
+
+
+}
